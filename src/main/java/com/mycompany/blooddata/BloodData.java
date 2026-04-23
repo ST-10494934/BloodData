d93478deb3e75409f680b64586c9d6e7d32fff08
ST10494934 <st10494934@myemeris.edu.za>
2026-04-23 10:02:06 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/blooddata/BloodData.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blooddata;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class BloodData {
+
+  // Fields to hold blood type and Rh factor
+    private String bloodType;
+    private String rhFactor;
+    
+    public BloodData(){
+        this.bloodType = "O";
+        this.rhFactor = "+";
+    }
+    
+    //Parameterized constructor 
+    
+}
